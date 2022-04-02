117e61a143befbda85b2271bfd5a418d9c17218c
Daniela Marin <daniela.marinmunoz@enersize.com>
2022-04-02 15:28:21 +0200

    added dynamic inputs

--- client/src/components/SecondTab.js
+++ client/src/components/SecondTab.js
@@ -5,6 +5,7 @@ import AudioPlayer from "../components/AudioPlayer";
 const SecondTab = () => {
   const [playlists, setPlaylists] = useState([]);
   const [currentTrack, setCurrentTrack] = useState();
+  const [counter, setCounter] = useState(0);
 
   const handlePlay = (track) => setCurrentTrack(track);
 
@@ -16,6 +17,10 @@ const SecondTab = () => {
     .then(res => res.json())  
     .then(res => console.log(res))
   };
+  const handleClick = () => {
+    setCounter(counter + 1); 
+  };
+
   useEffect(() => {
     fetch("http://localhost:8000/playlist")
       .then((res) => res.json())
@@ -24,6 +29,29 @@ const SecondTab = () => {
 
   return (
     <div className="SecondTab">
+      <form action="/create" method="post">
+        <ul>
+          <li>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="user_name" />
+          </li>
+          <li>
+            <label for="mail">E-mail:</label>
+            <input type="email" id="mail" name="user_email" />
+          </li>
+          <li>
+            <label for="msg">Message:</label>
+            <textarea id="msg" name="user_message"></textarea>
+          </li>
+          <li>
+            <button type="button" onClick={handleClick}>Hello</button>
+            {Array.from(Array(counter)).map((c, index) => {
+              return <input key={c} type="text"></input>;
+            })}
+          </li>
+        </ul>
+        <button type="submit"  value="Submit">Create</button>
+      </form>
       {playlists.map((playlist, ix) => (
            <div> 
              <label for="playlist">Playlist Name:</label> 
